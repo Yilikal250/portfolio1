a07d210cb32e38bf1d7af27d2add0af7e5ab80a3
Yilikal250 <teshomeyilikal250@gmail.com>
2023-01-31 00:01:25 +0300

    popup styles added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const mainMenue =document.querySelector('.mainMenu');
+const closeMenu =document.querySelector('.closeMenu');
+const openMenue =document.querySelector('.openMenu');
+
+
+openMenue.addEventListener('click',show);
+openMenue.addEventListener('click',close);
+
+function show() {
+   mainMenue.getElementsByClassName.display = 'flex';
+   mainMenue.getElementsByClassName.top ='0'
+}
+
+function close() {
+    mainMenue.style.top ='-100%'
+ }
+
 
